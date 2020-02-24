183730645e4d76a5abf65d0b622156b63adad8c8
Al-Hassan Abdel-Raouf <56361750+alhassanaraouf@users.noreply.github.com>
2020-02-24 05:48:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ from bottle import route, run, template
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
+
+@route('/')
+def test():
+    return template('<b>Hello, World</b>!')
+
+
+
 port = int(os.environ.get('PORT', 5000))
 
 run(host='0.0.0.0', port=port)
